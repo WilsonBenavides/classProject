a2b6319360b688ef55a2b3e9e4cf4d4259201d84
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-06 13:56:29 -0700

    first test running on console

---
+++ classRouteApp/classRouteApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  classRouteApp
+//
+//  Created by willix on 6/05/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("it's first running...")
+    }
+}
+
