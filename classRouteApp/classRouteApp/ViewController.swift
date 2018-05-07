ed20ea3f32bb89a53dab599bca8c2d76bcafa761
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-06 23:45:13 -0700

    complete background red display with safe area

--- classRouteApp/classRouteApp/ViewController.swift
+++ classRouteApp/classRouteApp/ViewController.swift
@@ -12,7 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("it's first running...")
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+//        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
     }
 }
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: top).isActive = true
+        leadingAnchor.constraint(equalTo: leading).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+    }
+}
+
+
+
+
+
 
