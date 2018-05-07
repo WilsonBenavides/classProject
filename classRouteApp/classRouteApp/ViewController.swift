1ab3b24671b023e90c5adb342e0eefeeef76e2dd
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-07 00:09:53 -0700

    create programatically view rectangle with constraints and padding and size

--- classRouteApp/classRouteApp/ViewController.swift
+++ classRouteApp/classRouteApp/ViewController.swift
@@ -20,23 +20,43 @@ class ViewController: UIViewController {
         
         redView.translatesAutoresizingMaskIntoConstraints = false
         
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 12), size: .init(width: 100, height: 100))
 //        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
 //        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
 //        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
 //        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+//        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16), size: .init(width: 100, height: 100))
     }
 }
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
-        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
-        bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
-        trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        
+        
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
 }
 
